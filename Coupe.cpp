b450962b1ec5f0a12a23299c731bdde3840b6bfb
Alexandru Nagy <nagyy.alexandru@gmail.com>
2024-03-21 10:31:39 +0200

    solved a bug in class Coupe

--- Coupe.cpp
+++ Coupe.cpp
@@ -6,13 +6,15 @@ int Coupe::objCount = 0;
 Coupe::Coupe(const std::string &combustibil, const std::string &tractiune,
              const std::string &echipare, int putere) : Automobil(combustibil,
                                                                   "coupe", tractiune, echipare, putere)
-{
-    std::cout << "Coupe()" << std::endl;
+{                                    
     ++objCount;
+
     if (objCount > COUPE_MAX_OBJ_COUNT)
     {
         throw std::logic_error("Error: Object creation exceeded for Coupe");
     }
+
+    std::cout << "Coupe()" << std::endl;
 }
 
 Coupe::Coupe(const Coupe &obj) : Automobil(obj)
