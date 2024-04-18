c890cfd33197299108b8cd07c797fec4389ba8b0
Alexandru Nagy <nagyy.alexandru@gmail.com>
2024-04-18 12:03:47 +0300

    updated Threads.cpp

--- Multithreading/Threads.cpp
+++ Multithreading/Threads.cpp
@@ -2,68 +2,51 @@
 #include <thread>
 #include <mutex>
 
-// Extern variable shared between threads
-int sharedVariable = 0;
+// Shared variable shared between threads
+int sharedVar;
 
-// Mutex to protect sharedVariable
+// Mutex for sync
 std::mutex mtx;
 
-// Function for the first thread
-void threadFunction1() {
-    for (int i = 0; i < 10; ++i) {
-        // Lock the mutex before accessing sharedVariable
+// Function to be executed by threads
+void doWork(int threadID)
+{
+    // Loop to perform operations on the shared variable
+    for(int i = 0; i < 5; ++i)
+    {
+        // Lock the mutex before accessing the shared variable
         mtx.lock();
 
-        // Read the sharedVariable
-        int value = sharedVariable;
+        // Read the shared variable
+        int value = sharedVar;
 
-        // Modify the sharedVariable
-        value += 1;
-        sharedVariable = value;
+        // Modify the shared variable
+        value += threadID;
+        sharedVar = value;
 
-        // Print the modified sharedVariable
-        std::cout << "Thread 1: Shared variable = " << sharedVariable << std::endl;
+        // Print the modified value
+        std::cout << "Thread " << threadID << ": Shared variable = " << sharedVar << std::endl;
 
-        // Unlock the mutex after finishing access
+        // Unlock the mutex after accessing the shared variable
         mtx.unlock();
 
-        // Sleep for a short time to simulate some work being done
+        // Simulate some background stuff
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
-// Function for the second thread
-void threadFunction2() {
-    for (int i = 0; i < 10; ++i) {
-        // Lock the mutex before accessing sharedVariable
-        mtx.lock();
+int main(void)
+{
+    // Initialize the shared, extern variable
+    sharedVar = 0;
 
-        // Read the sharedVariable
-        int value = sharedVariable;
-
-        // Modify the sharedVariable
-        value -= 1;
-        sharedVariable = value;
-
-        // Print the modified sharedVariable
-        std::cout << "Thread 2: Shared variable = " << sharedVariable << std::endl;
-
-        // Unlock the mutex after finishing access
-        mtx.unlock();
-
-        // Sleep for a short time to simulate some work being done
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-}
-
-int main() {
     // Create two threads
-    std::thread t1(threadFunction1);
-    std::thread t2(threadFunction2);
+    std::thread th1(doWork, 1);
+    std::thread th2(doWork, 2);
 
     // Join the threads with the main thread
-    t1.join();
-    t2.join();
+    th1.join();
+    th2.join();
 
     return 0;
 }
